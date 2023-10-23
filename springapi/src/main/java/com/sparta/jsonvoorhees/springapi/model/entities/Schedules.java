51c65699579b0553804891aacc53796fa33d6028
David K <davek032@outlook.com>
2023-10-23 12:38:29 +0100

    #2 Schedules done properly

--- springapi/src/main/java/com/sparta/jsonvoorhees/springapi/model/entities/Schedules.java
+++ springapi/src/main/java/com/sparta/jsonvoorhees/springapi/model/entities/Schedules.java
@@ -1,46 +1,49 @@
 package com.sparta.jsonvoorhees.springapi.model.entities;
 
-
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.util.Date;
 
 @Document("schedules")
 public class Schedules {
-    private String id;
-    private Date date;
-    private String movieId;
-    private String theaterId;
 
-    public String getId() {
-        return id;
-    }
+  private String id;
+  private String date;
+  private String movieId;
+  private String theaterId;
 
-    public void setId(String id) {
-        this.id = id;
-    }
 
-    public Date getDate() {
-        return date;
-    }
+  public String getId() {
+    return id;
+  }
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
+  public void setId(String id) {
+    this.id = id;
+  }
 
-    public String getMovieId() {
-        return movieId;
-    }
 
-    public void setMovieId(String movieId) {
-        this.movieId = movieId;
-    }
+  public String getDate() {
+    return date;
+  }
 
-    public String getTheaterId() {
-        return theaterId;
-    }
+  public void setDate(String date) {
+    this.date = date;
+  }
 
-    public void setTheaterId(String theaterId) {
-        this.theaterId = theaterId;
-    }
+
+  public String getMovieId() {
+    return movieId;
+  }
+
+  public void setMovieId(String movieId) {
+    this.movieId = movieId;
+  }
+
+
+  public String getTheaterId() {
+    return theaterId;
+  }
+
+  public void setTheaterId(String theaterId) {
+    this.theaterId = theaterId;
+  }
+
 }
