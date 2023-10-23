a96cdb07e5bae728f66226bf0daa894461bae944
David K <davek032@outlook.com>
2023-10-23 12:28:58 +0100

    #2 Fleshed out the schedule entity

--- springapi/src/main/java/com/sparta/jsonvoorhees/springapi/model/entities/Schedules.java
+++ springapi/src/main/java/com/sparta/jsonvoorhees/springapi/model/entities/Schedules.java
@@ -3,8 +3,44 @@ package com.sparta.jsonvoorhees.springapi.model.entities;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+
 @Document("schedules")
 public class Schedules {
-    
+    private String id;
+    private Date date;
+    private String movieId;
+    private String theaterId;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getTheaterId() {
+        return theaterId;
+    }
+
+    public void setTheaterId(String theaterId) {
+        this.theaterId = theaterId;
+    }
 }
