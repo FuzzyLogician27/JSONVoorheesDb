d8edfba0f6c495dbf87af3ccffb220acadc0e136
David K <davek032@outlook.com>
2023-10-23 12:19:53 +0100

    Added entities

---
+++ springapi/src/main/java/com/sparta/jsonvoorhees/springapi/model/entities/Schedules.java
@@ -0,0 +1,10 @@
+package com.sparta.jsonvoorhees.springapi.model.entities;
+
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("schedules")
+public class Schedules {
+    
+
+}
