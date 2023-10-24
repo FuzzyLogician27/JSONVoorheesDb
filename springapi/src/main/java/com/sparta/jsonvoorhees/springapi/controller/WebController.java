28e797cd17c66994dcf3ead426055f1e2855af31
Ruya Kumru-Holroyd <ruya.kh@outlook.com>
2023-10-24 16:48:12 +0100

    made separate controllers and their views

--- springapi/src/main/java/com/sparta/jsonvoorhees/springapi/controller/WebController.java
+++ springapi/src/main/java/com/sparta/jsonvoorhees/springapi/controller/WebController.java
@@ -20,45 +20,10 @@ public class WebController {
         this.commentRepository = commentRepository;
     }
 
-    @GetMapping("/web/movies")
-    public String getAllMovies(Model model, @RequestParam(name = "title", required = false) String title) {
-        model.addAttribute("movies", serviceLayer.getAllMoviesWithTitle(title));
-        return "movies";
-    }
-
-    @GetMapping("/web/movie/{id}")
-    public String getMovieById(Model model, @PathVariable String id) {
-        model.addAttribute("movie", serviceLayer.getMovieById(id).get());
-        model.addAttribute("comments",serviceLayer.getCommentsByMovie(id));
-        return "movie";
-    }
-
-    @GetMapping("/web/theaters")
-    public String getAllTheaters(Model model) {
-        model.addAttribute("theaters", serviceLayer.getAllTheaters());
-        return "theaters";
-    }
-
-    @GetMapping("/web/theater/{id}")
-    public String getTheaterById(Model model, @PathVariable String id) {
-        model.addAttribute("theater",serviceLayer.getTheaterById(id).get());
-        model.addAttribute("schedules",serviceLayer.getSchedulesForTheaters(id));
-        return "theater";
-    }
-
-    @GetMapping("/web/user/{id}")
-    public String getUserById(Model model, @PathVariable String id) {
-        model.addAttribute("user",serviceLayer.getUserById(id).get());
-        model.addAttribute("comments",serviceLayer.getCommentsByUser(id));
-        return "user";
-    }
-
     @GetMapping("/web/comment/{id}")
     public String getCommentsById(Model model, @PathVariable String id) {
-        //model.addAttribute("comments", serviceLayer.getCommentsByMovie(id));
         model.addAttribute("comments", serviceLayer.getCommentById(id).get());
         return "comment";
     }
-
 
 }
