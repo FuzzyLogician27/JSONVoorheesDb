b62f55f0e242592d899b026a495444975504f1a2
David K <davek032@outlook.com>
2023-10-25 14:00:39 +0100

    #6 First service layer test

--- springapi/src/test/java/com/sparta/jsonvoorhees/springapi/service/ApiLibraryServiceTests.java
+++ springapi/src/test/java/com/sparta/jsonvoorhees/springapi/service/ApiLibraryServiceTests.java
@@ -1,19 +1,67 @@
 package com.sparta.jsonvoorhees.springapi.service;
 
-import org.assertj.core.api.Assert;
+import com.sparta.jsonvoorhees.springapi.model.entities.*;
+import com.sparta.jsonvoorhees.springapi.model.entities.embeddedObjects.*;
+import com.sparta.jsonvoorhees.springapi.model.repositories.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class ApiLibraryServiceTests {
-    @Autowired
-    private ApiLibraryService testService;
+    @Mock
+    private static MovieRepository movieRepository;
+    @Mock
+    private static UserRepository userRepository;
+    @Mock
+    private static CommentRepository commentRepository;
+    @Mock
+    private static TheaterRepository theaterRepository;
+    @Mock
+    private static ScheduleRepository scheduleRepository;
+    @InjectMocks
+    private ServiceLayer serviceLayer;
+
+    @BeforeEach
+    void setUp()
+    {
+        serviceLayer = new ServiceLayer(commentRepository,
+                movieRepository,scheduleRepository,theaterRepository,
+                userRepository);
+
+    }
+
+    //Region Test Getters
+    @Test
+    public void testGetAllUsers()
+    {
+        assertEquals(serviceLayer.getAllUsers(), new ArrayList<User>());
+    }
 
     @Test
-    public void getAllFilms()
+    public void testGetUser()
     {
-        assertNotNull(testService.getMovieRepository().findAll());
+        User dummyUser = new User();
+        dummyUser.setName("Just a guy");
+        dummyUser.setEmail("name@domain.com");
+        dummyUser.setId("9999");
+        Mockito.when(userRepository.findUserById("9999")).thenReturn(Optional.of(dummyUser));
+
+        User testUserToRetrieve = serviceLayer.getUserById("9999").get();
+
+        assertEquals(dummyUser,testUserToRetrieve);
     }
+    //Endregion
 }
