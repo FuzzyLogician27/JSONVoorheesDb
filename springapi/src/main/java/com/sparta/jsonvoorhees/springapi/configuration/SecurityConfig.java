fe36dcce3ec7b9d950b268a3f82a795330f125e1
Mohammed Ali Butt <abutt2001@hotmail.com>
2023-10-27 10:39:49 +0100

    Security layer added

---
+++ springapi/src/main/java/com/sparta/jsonvoorhees/springapi/configuration/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.sparta.jsonvoorhees.springapi.configuration;
+
+import com.sparta.jsonvoorhees.springapi.service.MongoRoleUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final MongoRoleUserService mongoRoleUserService;
+
+    @Autowired
+    public SecurityConfig(MongoRoleUserService mongoRoleUserService) {
+        this.mongoRoleUserService = mongoRoleUserService;
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .userDetailsService(mongoRoleUserService)
+                .headers(headers -> headers.frameOptions().sameOrigin())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
