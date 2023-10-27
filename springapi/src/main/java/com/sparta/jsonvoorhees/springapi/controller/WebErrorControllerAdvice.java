2ae64c91280e510d7d62f0c77394f48dade213a0
Danyal <86931465+danymcdan0@users.noreply.github.com>
2023-10-27 14:39:49 +0100

    Added assignable types to controller advice so correct advice is given respective to api or web

--- springapi/src/main/java/com/sparta/jsonvoorhees/springapi/controller/WebErrorControllerAdvice.java
+++ springapi/src/main/java/com/sparta/jsonvoorhees/springapi/controller/WebErrorControllerAdvice.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ControllerAdvice
+@ControllerAdvice(assignableTypes = {HomeWebController.class, MovieWebController.class, TheaterWebController.class, UserWebController.class})
 public class WebErrorControllerAdvice {
     @ExceptionHandler(TheaterNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
